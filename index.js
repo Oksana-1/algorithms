c9028a28bd19d7eba56c61dbdf34779e77828253
oksana-1 <4438488@gmail.com>
2021-03-10 20:04:37 +0200

    Repeat a String Repeat a StringPassed (without String.prototype.repeat())

--- index.js
+++ index.js
@@ -2,19 +2,26 @@ export const convertToFahrenheit = (celsius) => {
   return (celsius * 9) / 5 + 32;
 };
 export const reverseString = (string) => {
-  return string.split('').reverse().join('');
-}
+  return string.split("").reverse().join("");
+};
 export const factorialize = (number) => {
   if (number === 0) return 1;
   return number * factorialize(number - 1);
-}
+};
 export const findLongestWordLength = (string) => {
-  const sortedWords = string.split(' ').sort((prev, next) => prev.length > next.length ? -1 : 1);
+  const sortedWords = string
+    .split(" ")
+    .sort((prev, next) => (prev.length > next.length ? -1 : 1));
   return sortedWords[0].length;
-}
+};
 export const largestOfFour = (array) => {
-  return array.map(innerArray => innerArray.sort((prev, next) => prev > next ? -1 : 1 )[0]);
-}
-export const  confirmEnding = (string, target) => {
+  return array.map(
+    (innerArray) => innerArray.sort((prev, next) => (prev > next ? -1 : 1))[0]
+  );
+};
+export const confirmEnding = (string, target) => {
   return Boolean(string.match(`${target}$`));
-}
+};
+export const repeatStringNumTimes = (str, num) => {
+  return num <= 0 ? "" : str + repeatStringNumTimes(str, num - 1);
+};
