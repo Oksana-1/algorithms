0a920e8a661205618d04c953ace61fb06789fc45
oksana-1 <4438488@gmail.com>
2021-03-18 19:38:21 +0200

    titleCase - description added

--- index.js
+++ index.js
@@ -37,6 +37,10 @@ export const findElement = (arr, func) => {
 export const isBoolean = (value) => {
   return typeof value === "boolean";
 };
+/*
+Return the provided string with the first letter of each word capitalized. Make sure the rest of the word is in lower case.
+For the purpose of this exercise, you should also capitalize connecting words like the and of.
+*/
 export const titleCase = (string) => {
   return string
     .split(" ")
