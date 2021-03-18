09bffd795b03c64362a1db3f144a53391d36aa25
oksana-1 <4438488@gmail.com>
2021-03-18 19:36:23 +0200

    Slice and SplicePassed - description added

--- index.js
+++ index.js
@@ -45,8 +45,14 @@ export const titleCase = (string) => {
     )
     .join(" ");
 };
+/*
+  You are given two arrays and an index.
+  Copy each element of the first array into the second array, in order.
+  Begin inserting elements at index n of the second array.
+  Return the resulting array. The input arrays should remain the same after the function runs.
+*/
 export const frankenSplice = (arr1, arr2, n) => {
   const result = [...arr2];
   result.splice(n, 0, ...arr1);
   return result;
-}
+};
