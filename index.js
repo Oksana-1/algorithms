9fe53c4c5ddfffc47becdef39df3ecec5424ae7f
oksana-1 <4438488@gmail.com>
2021-03-18 19:34:50 +0200

    Slice and SplicePassed

--- index.js
+++ index.js
@@ -45,3 +45,8 @@ export const titleCase = (string) => {
     )
     .join(" ");
 };
+export const frankenSplice = (arr1, arr2, n) => {
+  const result = [...arr2];
+  result.splice(n, 0, ...arr1);
+  return result;
+}
