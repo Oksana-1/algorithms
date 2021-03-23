edae4883f87423fefbd857904078e8edb881229e
oksana-1 <4438488@gmail.com>
2021-03-23 19:40:47 +0200

    MutationsPassed

--- index.js
+++ index.js
@@ -83,3 +83,16 @@ export const getIndexToIns = (arr, num) => {
   arr.sort((a, b) => a - b);
   return arr.indexOf(num);
 }
+/*
+Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
+For example, ["hello", "Hello"], should return true because all of the letters in the second string are present in the first, ignoring case.
+The arguments ["hello", "hey"] should return false because the string hello does not contain a y.
+Lastly, ["Alien", "line"], should return true because all of the letters in line are present in Alien.
+*/
+export const mutation = (array) => {
+  const letters =  array[1].toLowerCase().split('');
+  const word = array[0].toLowerCase();
+  return !letters.some(letter => word.indexOf(letter) === -1);
+}
+
+mutation(["hello", "hey"]);
