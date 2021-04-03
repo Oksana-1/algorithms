0cf82132abc36e4d65f3989aee3811994c9e9152
oksana-1 <4438488@gmail.com>
2021-04-03 21:51:23 +0300

    Add comments to Factorialize a Number

--- index.js
+++ index.js
@@ -12,6 +12,13 @@ Your result must be a string.*/
 export const reverseString = (string) => {
   return string.split("").reverse().join("");
 };
+/*
+Return the factorial of the provided integer.
+If the integer is represented with the letter n, a factorial is the product of all positive integers less than or equal to n.
+Factorials are often represented with the shorthand notation n!
+For example: 5! = 1 * 2 * 3 * 4 * 5 = 120
+Only integers greater than or equal to zero will be supplied to the function.
+*/
 export const factorialize = (number) => {
   if (number === 0) return 1;
   return number * factorialize(number - 1);
